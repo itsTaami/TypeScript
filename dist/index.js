11880f6a4c39e603209ba5e0a8d7505a55ceceeb
itsTaami <eegii0977@gmail.com>
2023-04-03 17:57:02 +0800

    Apr3

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const ner = "Boldoo";
+const age = 21;
+console.log("Name:", ner);
+console.log("Nas:", age);
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log("Sum", sum(5, 5));
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const ner: string = "Boldoo";
+const age: number = 21;
+
+console.log("Name:", ner);
+console.log("Nas:", age);
+
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+console.log("Sum", sum(5, 5));
