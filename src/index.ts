ead899c06aa96e5ee8322ec9123d04b5ad888ca9
itsTaami <eegii0977@gmail.com>
2023-04-04 17:47:16 +0800

    Apr4

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-const ner: string = "Boldoo";
-const age: number = 21;
+// const ner: string = "Boldoo";
+// const age: number = 21;
 
-console.log("Name:", ner);
-console.log("Nas:", age);
+// console.log("Name:", ner);
+// console.log("Nas:", age);
 
-function sum(num1: number, num2: number): number {
-  return num1 + num2;
+// function sum(num1: number, num2: number): number {
+//   return num1 + num2;
+// }
+// console.log("Sum", sum(5, 5));
+
+function logString(a: string): string {
+  console.log(a);
+  return a;
 }
-console.log("Sum", sum(5, 5));
+console.log(logString("Hello"));
+
+function logNumber(a: number): number {
+  console.log(a);
+  return a;
+}
+console.log(logNumber(30));
